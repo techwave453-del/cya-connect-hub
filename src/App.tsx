8a35450b29e44a4eb1500d8509982efafef2d76b
CYA Developer <dev@cya-connect.local>
2026-02-23 22:25:51 +0300

    chore: add webmanifest, mobile-web-app-capable meta; opt-in to react-router v7 future flags via RouterProvider

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route } from "react-router-dom";
 import { ThemeProvider } from "@/contexts/ThemeContext";
 import { OfflineProvider } from "@/contexts/OfflineContext";
 import Index from "./pages/Index";
@@ -29,19 +29,33 @@ const App = () => (
           <Toaster />
           <Sonner />
           <OfflineIndicator />
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<Index />} />
-              <Route path="/auth" element={<Auth />} />
-              <Route path="/profile/:userId" element={<ProfilePage />} />
-              <Route path="/chat" element={<ChatPage />} />
-              <Route path="/admin" element={<AdminPage />} />
-              <Route path="/games" element={<GamesPage />} />
-              <Route path="/install" element={<Install />} />
-              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </BrowserRouter>
+          {
+            // Create router with future flags to opt-in to v7 behaviors
+            (() => {
+              const routes = createRoutesFromElements(
+                <>
+                  <Route path="/" element={<Index />} />
+                  <Route path="/auth" element={<Auth />} />
+                  <Route path="/profile/:userId" element={<ProfilePage />} />
+                  <Route path="/chat" element={<ChatPage />} />
+                  <Route path="/admin" element={<AdminPage />} />
+                  <Route path="/games" element={<GamesPage />} />
+                  <Route path="/install" element={<Install />} />
+                  {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+                  <Route path="*" element={<NotFound />} />
+                </>
+              );
+
+              const router = createBrowserRouter(routes, {
+                future: {
+                  v7_startTransition: true,
+                  v7_relativeSplatPath: true,
+                },
+              });
+
+              return <RouterProvider router={router} />;
+            })()
+          }
         </TooltipProvider>
       </OfflineProvider>
     </ThemeProvider>
